5f01296a1eadd598a5b5094a26ceb85a80f03f2a
c0hb1rd <cohbird.zn@gmail.com>
2016-07-05 00:58:17 +0800

    add Show Image module

--- Final_Spider.py
+++ Final_Spider.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # coding:utf-8
 import requests as re
+import numpy
 import re as R
 import time
 import os
@@ -30,12 +31,12 @@ sys.setdefaultencoding('utf-8')
 
 # Default Cookie
 HEADERS = {
-    'Cookie':''
+    'Cookie':'SINAGLOBAL=5931281293742.358.1452508282462; __gads=ID=5a36146630fa668e:T=1458825990:S=ALNI_MZh1T3zAKd9ddVKsnokQpH9-GED_g; _ga=GA1.2.1442097102.1458825990; wb_g_minivideo_5447809712=1; wb_bub_hot_2331740335=1; wb_g_minivideo_2331740335=1; myuid=2331740335; wvr=6; wb_bub_hot_5447809712=1; TC-Page-G0=b05711a62e11e2c666cc954f2ef362fb; SSOLoginState=1467532906; _s_tentry=login.sina.com.cn; UOR=,,login.sina.com.cn; Apache=4265928568177.777.1467532910175; ULV=1467532910258:272:3:1:4265928568177.777.1467532910175:1467424294699; SCF=AlU6pua4h8VAFCCSWk9ptfy9a56Wr8cfJw4pGFS5IKhER1TGEw0f-8VBs3JN-dkUA0kH181489YgEGOOtwlYWSw.; SUB=_2A256fLdKDeTxGeNK71UZ8CfLyj6IHXVZC6-CrDV8PUJbmtAKLVTWkW8PI200aJP_IStbg1ZuxChY0qDFHw..; SUBP=0033WrSXqPxfM725Ws9jqgMF55529P9D9WWlXurN77AppfK3I6VNbye25JpX5o2p5NHD95QfShBN1h54S02EWs4DqcjGq0WQqo8E95tt; SUHB=0HTRL-q4uUrol6; ALF=1499065531; TC-V5-G0=866fef700b11606a930f0b3297300d95; WBStore=8ca40a3ef06ad7b2|undefined; TC-Ugrow-G0=5e22903358df63c5e3fd2c757419b456'
 }
 
 # Cookie For URL: http://m.weibo.cn/users/-*-UserID-*-
 HEADERS_FOR_GET_INFO = {
-    'Cookie':''
+    'Cookie':'_T_WM=1867af942c92a23704b094e55fbbfadb; ALF=1470210119; SCF=AlU6pua4h8VAFCCSWk9ptfy9a56Wr8cfJw4pGFS5IKhE8EkPUUOqCty3khnptxk4TRzSm7OF4d7gBVikZzFr0UI.; SUB=_2A256fldEDeTxGeNK71UZ8CfLyj6IHXVZgXkMrDV6PUNbktAKLUigkW1u0NCLekCLCmV8873TAtgW51HJ9A..; SUBP=0033WrSXqPxfM725Ws9jqgMF55529P9D9WWlXurN77AppfK3I6VNbye25JpX5KMhUgL.Fo-XShMReh.NeKz2dJLoI7UoesybePi.; SUHB=0BLYOIro2bXNqF; SSOLoginState=1467623188; gsid_CTandWM=4uA7CpOz5c6o1q6dEOpDBmRdTbu'
 }
 
 
@@ -55,7 +56,7 @@ def getImageLinks(uid, page, headers=HEADERS):
             data = json.loads(content)
 
             for src in data['cards'][0]['card_group'][0]['pics']:
-                imgLinks.append(src['pic_ori'])
+                imgLinks.append(src['pic_ori'].replace('large', 'thumb300'))
 
             return imgLinks
         except:
@@ -117,12 +118,13 @@ def detectFaces(imageName):
         return result
 
 
-# Judge The User Fans Greater Than 'fans'
-def isGreaterThan(info, fans):
-    if info['粉丝'] > fans:
-        return True
-    else:
-        return False
+# Show Image
+def showImage(w_title, url):
+    rep = re.get(url, stream=True)
+    image = numpy.asarray(bytearray(rep.content), dtype='uint8')
+    image = cv2.imdecode(image, cv2.IMREAD_COLOR)
+    cv2.imshow("Image", image)
+    cv2.waitKey(0)
 
 
 # Get User Information, Return Dict
@@ -143,25 +145,36 @@ def getUserInformation(uid, headers=HEADERS_FOR_GET_INFO):
         elif key[-12:] == '微博认证':
             key = key[-12:]
         ls[key] = value
-    ls['粉丝'] = getUserFans(uid)
     ls['UID'] = uid
     return ls
+
+
+# Show Image
+def showImage(w_title, url):
+    rep = re.get(url, stream=True)
+    image = numpy.asarray(bytearray(rep.content), dtype='uint8')
+    image = cv2.imdecode(image, cv2.IMREAD_COLOR)
+    cv2.imshow("Image", image)
+    cv2.waitKey(delay=3000)
 
 # 1075567392
 # 1774543811
 
 def main(uid):
     information = getUserInformation(uid)
-    imgLinks = getImageLinks(uid, 1)
+    imgLinks1 = getImageLinks(uid, 1)
+    imgLinks2 = getImageLinks(uid, 2)
+    imgLinks = imgLinks1 + imgLinks2
     if isGreaterThan(information, 500):
-        for link in imgLinks:
-            print link
         for k, v in information.items():
             print k, ':', v
+        for link in imgLinks:
+            showImage("Face", link)
     else:
         print 'no'
     print
 
-Thread(target=main, args=(1774543811, )).start()
-Thread(target=main, args=(1075567392, )).start()
-Thread(target=main, args=(5447809712, )).start()
+# Thread(target=main, args=(1774543811, )).start()
+# Thread(target=main, args=(1075567392, )).start()
+# Thread(target=main, args=(5447809712, )).start()
+print getUserFans(1000000002)
